de2564f87ed680bc6107f5a09b5b380506f67b21
ArturAzarov <artur.azarau@yandex.ru>
2016-02-29 18:08:10 +0300

    Сделанная задача

--- Task_1/Task_1/Program.cs
+++ Task_1/Task_1/Program.cs
@@ -10,12 +10,30 @@ namespace Task_1
     {
         static void Main(string[] args)
         {
-            
-        }
-
-        static int func(int x);
-        {
-            
+            Console.WriteLine("Введите количество элементов");
+            int n = int.Parse(Console.ReadLine());            
+            int[] array = new int[n];
+            int sum1 = 0, sum2 = 0;
+            string[] arr = (Console.ReadLine()).Split(' ');
+            for (int i = 0; i < arr.Length; i++)
+            {
+                array[i] = Convert.ToInt32(arr[i]);
+            }
+            Array.Sort(array);
+            for (int j = n-1; j>=0; j--)
+            {
+                if (sum1 - sum2 >= 0)
+                {
+                    sum2 += array[j];
+                }
+                else
+                {
+                    sum1 += array[j];
+                }
+            }
+            Console.WriteLine("Минимальная разность равна " + (sum2-sum1));
+            Console.ReadLine();
         }
     }
 }
+
