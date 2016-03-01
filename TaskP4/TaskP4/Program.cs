0fe290c7a3b1db7e8031931b0f63b1b675e3c3bc
ArturAzarov <artur.azarau@yandex.ru>
2016-03-01 17:42:40 +0300

    Паскаль №4

---
+++ TaskP4/TaskP4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskP4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            Console.WriteLine(str[2]+""+str[1]+""+str[0]);
+        }
+    }
+}
