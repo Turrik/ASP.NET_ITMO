ebcfef327aeb999e43d82f10fde28e75af542f6f
ArturAzarov <artur.azarau@yandex.ru>
2016-03-01 18:11:56 +0300

    Паскаль №7

---
+++ TaskP7/TaskP7/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskP7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            if (a > b && a > c)
+            {
+                Console.WriteLine(a);
+
+            }
+            else if (b > a && b > c)
+            {
+                Console.WriteLine(b);
+
+            }
+            else if (c > a && c > b)
+            {
+                Console.WriteLine(c);
+
+            }
+
+
+        }
+    }
+}
